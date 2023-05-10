f224b3a89f4ce8b0fcf5e8bc08d7b90e50a4e496
JHM <janhendrik.mathes@student.uni-luebeck.de>
2023-05-10 23:39:32 +0200

    Fix Link forPowerInput

--- src/main/java/com/tryouts/restapi/processor/PowerInputTypeProcessor.java
+++ src/main/java/com/tryouts/restapi/processor/PowerInputTypeProcessor.java
@@ -1,22 +1,23 @@
 package com.tryouts.restapi.processor;
 
 import com.tryouts.restapi.controller.DistrictController;
+import com.tryouts.restapi.controller.PowerInputTypeController;
 import com.tryouts.restapi.model.PowerInput;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.LinkRelation;
 import org.springframework.hateoas.server.RepresentationModelProcessor;
+import org.springframework.lang.NonNull;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 public class PowerInputTypeProcessor implements RepresentationModelProcessor<EntityModel<PowerInput>> {
-
+    @NonNull
     @Override
-    public EntityModel<PowerInput> process(EntityModel<PowerInput> model) {
+    public EntityModel<PowerInput> process(@NonNull EntityModel<PowerInput> model) {
         if (model.getContent() != null) {
             model.add(
-                    Link.of("/powerInputType/{powerInputTypeID}").withRel(LinkRelation.of("powerInputType")) //
+                    linkTo(methodOn(PowerInputTypeController.class).findByID(model.getContent().getPowerInputType().getId())).withRel(LinkRelation.of("powerInputType")) //
                             .expand(model.getContent().getPowerInputType().getId()));
             model.add(
                     linkTo(methodOn(DistrictController.class).findByID(model.getContent().getDistrict().getId()))
